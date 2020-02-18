30a1d2f122a47b614a1d72698671ef13e0c062e1
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-02-18 19:02:03 +0000

    Created folders, added scope, methods, etc

---
+++ scope/scope.js
@@ -0,0 +1,65 @@
+// global variable - can be accessed by any other js running on the page
+const first = `gearoid`;
+
+const age = 100;
+function howsThings() {
+  const well = `howya`;
+}
+
+console.log(age);
+// console.log(well);
+// this is function scope
+// you can call the function to access well
+
+function howOldAmI() {
+  const name = `Helen`;
+  console.log(age);
+  console.log(name);
+}
+howOldAmI();
+// if a function cannot find a variable within the function it will go up a level higher(found age from above)
+
+// if (1997 === 1997) {
+//     let upTheBanner = true
+// }
+// console.log(upTheBanner);
+// { } is a block, this will not work as upTheBanner is within the block and not global scope
+
+let upTheBanner;
+if (1997 === 1997) {
+  upTheBanner = true;
+}
+console.log(upTheBanner);
+// create the variable above and then update it in the block upTheBanner = true
+
+// //for(var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// because this is var there is a random value for i outside the scope
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+// using let it means there is no value for i outside of the block
+// var variables are funtion scoped, let & const variables are block scoped
+
+// const dog = `sally`;
+
+// function logDog() {
+//   console.log(dog);
+// }
+
+// function go() {
+//   const dog = `rover`;
+//   logdog();
+// }
+
+// go();
+// consoles sally, it is taken from where it is designed not inside the function - logdog its sally so it stays at sally
+
+function sayHello(name) {
+  function yell() {
+    console.log(name.toUpperCase());
+  }
+  yell();
+}
+sayHello(`gearoid`);
