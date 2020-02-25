e15fa9e09ed54fcd31543e04ba987cb3674a840b
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-02-25 14:12:03 +0000

    prettier

--- etch-a-sketch/etch33.js
+++ etch-a-sketch/etch33.js
@@ -1,73 +1,71 @@
+const canvas = document.querySelector("#etch-a-sketch");
+const ctx = canvas.getContext("2d");
 
-const canvas = document.querySelector('#etch-a-sketch');
-const ctx = canvas.getContext('2d');
-const shakebutton = document.querySelector('.shake');
+const shakebutton = document.querySelector(".shake");
 const MOVE_AMOUNT = 10;
-
 
 const { width, height } = canvas;
 
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
-
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
 ctx.lineWidth = MOVE_AMOUNT;
 
- let hue = 0;
- ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-ctx.beginPath(); 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
 function draw({ key }) {
-   hue = hue + 10;
-   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    console.log(key);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    switch (key) {
-        case 'ArrowUp':
-          y -= MOVE_AMOUNT;
-          break;
-        case 'ArrowRight':
-          x += MOVE_AMOUNT;
-          break;
-        case 'ArrowDown':
-          y += MOVE_AMOUNT;
-          break;
-        case 'ArrowLeft':
-          x -= MOVE_AMOUNT;
-          break;
-        default:
-          break;
-      }
-ctx.lineTo(x, y);
-ctx.stroke();
-
+  hue = hue + 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  console.log(key);
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  switch (key) {
+    case "ArrowUp":
+      y -= MOVE_AMOUNT;
+      break;
+    case "ArrowRight":
+      x += MOVE_AMOUNT;
+      break;
+    case "ArrowDown":
+      y += MOVE_AMOUNT;
+      break;
+    case "ArrowLeft":
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
 }
 
 function handleKey(e) {
-if (e.key.includes('Arrow')) {
+  if (e.key.includes("Arrow")) {
     e.preventDefault();
-    draw({ key: e.key});
-
-}   
-}
-function clearCanvas() {
-    canvas.classList.add('shake');
-    ctx.clearRect(0, 0, width, height);
-    canvas.addEventListener(
-      'animationend',
-      function() {
-      
-        canvas.classList.remove('shake');
-      },
-      { once: true }
-    );
+    draw({ key: e.key });
   }
+}
 
-window.addEventListener('keydown', handleKey);
-shakebutton.addEventListener('click', clearCanvas);
+function clearCanvas() {
+  canvas.classList.add("shake");
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    "animationend",
+    function() {
+      canvas.classList.remove("shake");
+    },
+    { once: true }
+  );
+}
+
+window.addEventListener("keydown", handleKey);
+shakebutton.addEventListener("click", clearCanvas);
+
+function drawSquare()
