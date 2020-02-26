db73f9a231c01ce9b44cb86311679d0e00943f35
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-02-26 17:44:46 +0000

    finished scroll to accept

--- scrolltoaccept35/scrolltoaccept35.js
+++ scrolltoaccept35/scrolltoaccept35.js
@@ -1,6 +1,17 @@
-const terms = document.querySelector(`.terms-and-conditions`);
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
 
-terms.addEventListener(`scroll`, function(e) {
-  console.log(e.currentTarget.scrollTop);
-  console.log(e.currentTarget.scrollHeight);
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
 });
+
+ob.observe(terms.lastElementChild);
