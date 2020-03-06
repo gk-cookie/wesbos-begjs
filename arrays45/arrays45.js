37fba9741cf9cbe9ab0f39155650a14e5132effb
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-06 13:27:10 +0000

    Arrays 45 video from Wesbos and examples

---
+++ arrays45/arrays45.js
@@ -0,0 +1,67 @@
+const names = [`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`,
+`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`,
+`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`,
+`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`,
+`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`,
+`Martin`, `Mike`, `Joe`, `Gearoid`, `Brian`];
+
+console.log(names[16]);
+console.log(names.length);
+console.log(names[names.length - 5]);
+
+names.push(`Gerard`);
+names.unshift(`Helen`);
+names.splice(4,6);
+console.log(names);
+
+const names2 = [`Helen2`, ...names];
+console.log(names2);
+
+const names3 = [...names2.slice(0, 5), `Gerard2`, ...names2.slice(11)];
+console.log(names3);    
+
+const names4 = [ ...names3.slice(0,5), ...names3.slice(7)];
+console.log(names4);
+
+
+// Mutation Method
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+/*const numbersBackwards = *//* numbers.reverse(); *//* Do not need to create a new const */
+// console.log(numbersBackwards); The numbers array has mutated to numbersBackwards
+console.log(numbers);
+
+//Immutable - They do not change the original data
+const pizzaSlice = numbers.slice(3, 7);
+console.log(pizzaSlice);
+console.log(numbers);
+
+const namesAge = [
+    { name: `Marty`, age: 40},
+    { name: `Mike`, age: 39},
+    { name: `Joey`, age: 36},
+    { name: `Jimmy`, age: 35},
+    { name: `Cookie`, age: 34},
+    { name: `Briano`, age: 27}
+];
+
+//Static Methods
+Array.of(`Cooraclare`, `Cree`);
+[...`Cooraclare`];
+
+const range = Array.from({ length: 10}, function(){
+    return `Cookie`;
+});
+
+console.log(range);
+
+function createRange(start, end) {
+    const range1 = Array.from({ length: end - start}, function(item, index){
+        return index = start;
+    });
+    return range1;  
+}
+createRange(3, 11);
+
+
