94e1c9d7f1d8dcd21b826f175103dec6e0ffc898
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-19 12:43:02 +0000

    Changes

---
+++ arrays45/forloops49.js
@@ -0,0 +1,30 @@
+const toppings = ['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese'];
+
+
+function logTopping(topping, index, originalArray) {
+    console.log(topping, index, originalArray);
+
+    const nextTopping = originalArray[index + 1];
+    const prevTopping = originalArray[index - 1];
+
+    nextTopping ? console.log(nextTopping) : `null`;
+    prevTopping ? console.log(prevTopping) : `null`;
+    
+    index === originalArray.length -1
+    ? console.log(`You are done`)
+    : console.log(`getting next topping`);
+    
+
+    console.log(`------pizza--------`);
+    
+    
+
+};
+
+
+toppings.forEach(logTopping);
+
+toppings.forEach(topping => {
+    console.log(topping);
+});
+
