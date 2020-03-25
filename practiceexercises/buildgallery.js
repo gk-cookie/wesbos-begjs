b5059d2049730b64f8790fc1efb2fc1e4efb20b7
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-25 12:54:23 +0000

    Work in progress

--- practiceexercises/buildgallery.js
+++ practiceexercises/buildgallery.js
@@ -1,7 +1,52 @@
 function Gallery(gallery) {
-    
-   
-    }
+  if (!gallery) {
+    throw new Error("No Gallery Found");
+  }
+  const images = Array.from(gallery.querySelectorAll("img"));
+  console.log(images);
+  const modal = document.querySelector(".modal");
+  const prevButton = modal.querySelector(".prev");
+  const nextButton = modal.querySelector(".next");
+  let currentImage;
 
-    const gallery1 = Gallery(document.querySelector('.gallery1'));
-    const gallery2 = Gallery(document.querySelector('.gallery2'));
+  function openModal() {
+    console.info("Opening Modal");
+    if (modal.matches(".open")) {
+      console.log("Modal already open");
+      return;
+    }
+    modal.classList.add("open");
+  }
+
+  function closeModal() {
+    modal.classList.remove("open");
+
+    function handleClickOutside(e) {
+        if (e.target === e.currentTarget) {
+          closeModal();
+        }
+    }
+  }
+
+  function showImage(el) {
+    if (!el) {
+      console.info("no image to show");
+      return;
+    }
+    console.log(el);
+    modal.querySelector("img").src = el.src;
+    modal.querySelector("h2").textContent = el.title;
+    modal.querySelector("figure p").textContent = el.dataset.description;
+    currentImage = el;
+
+    openModal();
+  }
+
+  images.forEach(image =>
+    image.addEventListener("click", showImage(e.currentTarget))
+  );
+  modal.addEventListener("click", handleClickOutside);
+}
+
+const gallery1 = Gallery(document.querySelector(".gallery1"));
+const gallery2 = Gallery(document.querySelector(".gallery2"));
