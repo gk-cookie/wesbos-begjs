d6ea388b33490ffdab403861948280a57a973cf4
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-25 11:06:49 +0000

    Added Photos and HTML file for Gallery

--- practiceexercises/buildgallery.js
+++ practiceexercises/buildgallery.js
@@ -1,1 +1,7 @@
+function Gallery(gallery) {
+    
+   
+    }
 
+    const gallery1 = Gallery(document.querySelector('.gallery1'));
+    const gallery2 = Gallery(document.querySelector('.gallery2'));
