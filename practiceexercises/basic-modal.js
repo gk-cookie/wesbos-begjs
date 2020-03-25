7de784a5d77d24d418dc74f10ad850bba300e54e
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-25 16:18:49 +0000

    Adding and removing the modal when the button is pressed

--- practiceexercises/basic-modal.js
+++ practiceexercises/basic-modal.js
@@ -1,7 +1,12 @@
 const buttonPressed = document.querySelector(".button");
+const modal = document.querySelector(".modal");
 
 function buttPress() {
   console.log("button pressed");
+  if (modal.classList.contains("open")) {
+    document.querySelector(".modal").classList.remove("open");
+  } else {
+    document.querySelector(".modal").classList.add("open");
+  }
 }
 buttonPressed.addEventListener("click", buttPress);
-
