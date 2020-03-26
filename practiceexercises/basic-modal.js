ca8bfc610ea2be5f4d8df21537ec9dd7580c7baa
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-26 15:09:01 +0000

    Click on pictures, opens modal and eblarges pictures. Updated shortcuts

--- practiceexercises/basic-modal.js
+++ practiceexercises/basic-modal.js
@@ -5,11 +5,22 @@ const pictures = document.querySelectorAll(".picture");
 function picClick() {
   console.log(`picture clicked`);
 }
+function openModal() {
+  document.querySelector(".modal").classList.add("open");
+  console.log(`modal Open`);
+  
+}
 
 function handlePictureButtonClick(event) {
   const button = event.currentTarget;
   const picture = button.closest(".picture");
-  console.log(picture);
+  const imgSrc = picture.querySelector("img").src;
+  console.log(imgSrc);
+  const inner = `<img src="${imgSrc.replace("300", "600")}" />`;
+  console.log(inner);
+  
+  modal.innerHTML = inner;
+  openModal();
 }
 
 pictures.forEach(button =>
