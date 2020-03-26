14d19f3cbb00619a88f41c8d02a4c9edfa60c12b
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-26 11:24:06 +0000

    changed values to this., took functions outside orignal gallery function

--- practiceexercises/gallery-proto-62.js
+++ practiceexercises/gallery-proto-62.js
@@ -2,68 +2,73 @@ function Gallery(gallery) {
   if (!gallery) {
     throw new Error("No Gallery Found");
   }
-  const images = Array.from(gallery.querySelectorAll("img"));
-  console.log(images);
-  const modal = document.querySelector(".modal");
-  const prevButton = modal.querySelector(".prev");
-  const nextButton = modal.querySelector(".next");
-  let currentImage;
+ 
+  this.gallery = gallery;
+  this.images = Array.from(gallery.querySelectorAll("img"));
+  console.log(this.images);
+  this.modal = document.querySelector(".modal");
+  this.prevButton = this.modal.querySelector(".prev");
+  this.nextButton = this.modal.querySelector(".next");
+  
+  
 
-  function openModal() {
-    console.info("Opening Modal");
-    if (modal.matches(".open")) {
-      console.log("Modal already open");
-      return;
-    }
-    modal.classList.add("open");
-    window.addEventListener("keyup", handleKeyUp);
-    nextButton.addEventListener("click", showNextImage);
-    prevButton.addEventListener("click", showPrevImage);
-  }
-
-  function closeModal() {
-    modal.classList.remove("open");
-    window.removeEventListener("keyup", handleKeyUp);
-    nextButton.removeEventListener("click", showNextImage);
-    prevButton.removeEventListener("click", showPrevImage);
-  }
-  function handleClickOutside(e) {
-    if (e.target === e.currentTarget) {
-      closeModal();
-    }
-  }
-  function handleKeyUp(event) {
-    if (event.key === "Escape") return closeModal();
-    if (event.key === "ArrowRight") return showNextImage();
-    if (event.key === "ArrowLeft") return showPrevImage();
-  }
-  function showNextImage() {
-    showImage(currentImage.nextElementSibling || gallery.firstElementChild);
-  }
-
-  function showPrevImage() {
-    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
-  }
-
-  function showImage(el) {
-    if (!el) {
-      console.info("no image to show");
-      return;
-    }
-    console.log(el);
-    modal.querySelector("img").src = el.src;
-    modal.querySelector("h2").textContent = el.title;
-    modal.querySelector("figure p").textContent = el.dataset.description;
-    currentImage = el;
-
-    openModal();
-  }
-
-  images.forEach(image =>
+  this.images.forEach(image =>
     image.addEventListener("click", e => showImage(e.currentTarget))
   );
-  modal.addEventListener("click", handleClickOutside);
+  this.modal.addEventListener("click", handleClickOutside);
 }
 
-const gallery1 = Gallery(document.querySelector(".gallery1"));
-const gallery2 = Gallery(document.querySelector(".gallery2"));
+Gallery.prototype.openModal = function() {
+  console.info("Opening Modal");
+  if (this.modal.matches(".open")) {
+    console.log("Modal already open");
+    return;
+  }
+  this.modal.classList.add("open");
+  window.addEventListener("keyup", handleKeyUp);
+  this.nextButton.addEventListener("click", showNextImage);
+  this.prevButton.addEventListener("click", showPrevImage);
+}
+
+function closeModal() {
+  this.modal.classList.remove("open");
+  window.removeEventListener("keyup", handleKeyUp);
+  this.nextButton.removeEventListener("click", showNextImage);
+  this.prevButton.removeEventListener("click", showPrevImage);
+}
+function handleClickOutside(e) {
+  if (e.target === e.currentTarget) {
+    closeModal();
+  }
+}
+function handleKeyUp(event) {
+  if (event.key === "Escape") return closeModal();
+  if (event.key === "ArrowRight") return showNextImage();
+  if (event.key === "ArrowLeft") return showPrevImage();
+}
+function showNextImage() {
+  showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+}
+
+function showPrevImage() {
+  showImage(currentImage.previousElementSibling || gallery.lastElementChild);
+}
+
+function showImage(el) {
+  if (!el) {
+    console.info("no image to show");
+    return;
+  }
+  console.log(el);
+  this.modal.querySelector("img").src = el.src;
+  this.modal.querySelector("h2").textContent = el.title;
+  this.modal.querySelector("figure p").textContent = el.dataset.description;
+  this.currentImage = el;
+
+  openModal();
+}
+
+const gallery1 = new Gallery(document.querySelector(".gallery1"));
+const gallery2 = new Gallery(document.querySelector(".gallery2"));
+
+console.log(gallery1, gallery2);
