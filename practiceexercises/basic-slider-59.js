6f7079d2604c95e562ae96b2329ecd527a69f7d0
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-30 11:07:12 +0100

    functions added to update current, prev, next and add classList

--- practiceexercises/basic-slider-59.js
+++ practiceexercises/basic-slider-59.js
@@ -3,12 +3,27 @@ function Slider(slider) {
     throw new Error("No Slider passed in");
   }
   let current;
-  let previous;
+  let prev;
   let next;
 
-  const slides = slider.querySelectorAll(".slides");
+  const slides = slider.querySelector(".slides");
   const prevButton = document.querySelector(".goToPrev");
   const nextButton = document.querySelector(".goToNext");
+
+  function startSlider() {
+    current = slider.querySelector(".current") || slides.firstElementChild;
+    prev = current.previousElementSibling || slides.lastElementChild;
+    next = current.nextElementSibling || slides.firstElementChild;
+    console.log(current, prev, next);
+  }
+
+  function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next');
+  }
+  startSlider();
+  applyClasses();
 }
 
 const mySlider = Slider(document.querySelector(".slider"));
