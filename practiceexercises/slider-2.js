086b8d6659e621a28edbd3ebba4f6c4a714f5e42
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-30 15:02:46 +0100

    Created css, html and js from scratch

---
+++ practiceexercises/slider-2.js
@@ -0,0 +1,13 @@
+function Slider() {
+    
+    let prev;
+    let current;
+    let next;
+
+    const slides = slider.querySelector(".slides");
+    const prevButton = slider.querySelector(".goToPrev");
+    const nextButton = slider.querySelector(".goToNext");
+    
+}
+
+const mySlider = Slider(document.querySelector(".slider"));
