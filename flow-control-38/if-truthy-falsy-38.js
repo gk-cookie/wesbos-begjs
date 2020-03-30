ae3c838edeb8750875e7823d9e6a6a0a7c498a54
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-30 18:27:11 +0100

    If statements

--- flow-control-38/if-truthy-falsy-38.js
+++ flow-control-38/if-truthy-falsy-38.js
@@ -1,1 +1,39 @@
-console.log('working');
+const age = 52;
+
+if (age > 70) {
+  console.log(" In your seventies");
+} else if (age > 60) {
+  console.log("In your sixties");
+} else if (age > 50) {
+  console.log("In your fifties");
+} else {
+  console.log(`Nothing was true`);
+}
+
+function slugify(sentence, lowercase) {
+  if (lowercase) {
+    return sentence.replace(/\ s/g, "-").toLowerCase();
+  }
+  return sentence.replace(/\ s/g, "-");
+}
+
+const name = "cookie";
+const last = "kelly";
+
+if (name === "cookie" || last === "collins") {
+  console.log("at least half the name is right");
+}
+
+if (name === "cookie" && last === "kelly") {
+  console.log("The full name is right");
+}
+
+const place = "cooraclare";
+
+if (place.includes("clare")) {
+  console.log(place);
+}
+
+// includes method returns a boolean
+
+
