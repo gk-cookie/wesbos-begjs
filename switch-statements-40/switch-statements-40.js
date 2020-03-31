9ad1c2ed64e41a25b05d2c6f751504f19f8e000e
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-31 12:04:52 +0100

    Turtle moving with keys, used --x/y property for the css to move turtle

--- switch-statements-40/switch-statements-40.js
+++ switch-statements-40/switch-statements-40.js
@@ -1,9 +1,29 @@
 const turtle = document.querySelector(".turt");
+let x = 0;
+let y = 0;
+const speed = 5;
+
 function handleKeyDown(event) {
   if (!event.key.includes("Arrow")) {
     return;
   }
-  console.log(event.key);
+  switch (event.key) {
+    case "ArrowUp":
+      y -= 1;
+      break;
+    case "ArrowDown":
+      y += 1;
+      break;
+    case "ArrowLeft":
+      x -= 1;
+      break;
+    case "ArrowRight":
+      x += 1;
+      break;
+    default:
+      console.log("That is not a valid move");
+  }
+  turtle.setAttribute("style", `--x: ${x * speed}px; --y: ${y * speed}px;`);
 }
 
 window.addEventListener("keydown", handleKeyDown);
