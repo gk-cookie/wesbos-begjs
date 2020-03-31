c53af40f861236c417b9cb1a027c6c73de734289
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-03-31 12:59:45 +0100

    working movement on the turtle and also flips in the desired direction

--- switch-statements-40/switch-statements-40.js
+++ switch-statements-40/switch-statements-40.js
@@ -2,6 +2,8 @@ const turtle = document.querySelector(".turt");
 let x = 0;
 let y = 0;
 const speed = 5;
+let flipped = false;
+let rotate = 0;
 
 function handleKeyDown(event) {
   if (!event.key.includes("Arrow")) {
@@ -10,20 +12,33 @@ function handleKeyDown(event) {
   switch (event.key) {
     case "ArrowUp":
       y -= 1;
+      rotate = -90;
       break;
     case "ArrowDown":
       y += 1;
+      rotate = 90;
       break;
     case "ArrowLeft":
       x -= 1;
+      flipped = true;
+      rotate = 0;
       break;
     case "ArrowRight":
       x += 1;
+      flipped = false;
+      rotate = 0;
       break;
     default:
       console.log("That is not a valid move");
   }
-  turtle.setAttribute("style", `--x: ${x * speed}px; --y: ${y * speed}px;`);
+  turtle.setAttribute(
+    "style",
+    `
+  --x: ${x * speed}px;
+  --y: ${y * speed}px;
+  --rotateX: ${flipped ? '180deg' : '0'};
+  --rotate: ${rotate}deg; 
+   `);
 }
 
 window.addEventListener("keydown", handleKeyDown);
