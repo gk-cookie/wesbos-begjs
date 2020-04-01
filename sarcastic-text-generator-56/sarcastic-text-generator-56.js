2c18dbeb4156b26db03bfb0a37a7d126e98b1107
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-01 11:07:44 +0100

    added event listener to get text when typed for use on generator

--- sarcastic-text-generator-56/sarcastic-text-generator-56.js
+++ sarcastic-text-generator-56/sarcastic-text-generator-56.js
@@ -1,1 +1,14 @@
-console.log(`it works`);
+const textarea = document.querySelector('[name="text"]');
+const result = document.querySelector(".result");
+const filterInputs = document.querySelectorAll('[name="filter"]');
+
+console.log(textarea);
+console.log(result);
+console.log(filterInputs);
+
+function transformText(text) {
+  console.log(text);
+  result.textContent = text;
+}
+
+textarea.addEventListener("input", e => transformText(e.target.value));
