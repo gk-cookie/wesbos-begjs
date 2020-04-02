46c6269ad462d1b17789b6ecc5146d39b44de38d
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-02 11:13:22 +0100

    bind examples and explanations

--- bind-call-apply-65/bind-call-apply-65.js
+++ bind-call-apply-65/bind-call-apply-65.js
@@ -12,10 +12,13 @@ function Pizza(toppings = [], customer) {
   console.log("Making a Pizza");
   this.toppings = toppings;
   this.customer = customer;
-  this.id = Math.floor(Math.random()*16777215).toString(16); 
+  this.id = Math.floor(Math.random() * 16777215).toString(16);
 }
-const supremePizza = new Pizza(['olives', 'mushroom', 'chicken'], 'Cookie Kelly');
-const chessePizza = new Pizza(['cheddar', 'mozz', 'swiss'], 'Tommy Kelly');
+const supremePizza = new Pizza(
+  ["olives", "mushroom", "chicken"],
+  "Cookie Kelly"
+);
+const chessePizza = new Pizza(["cheddar", "mozz", "swiss"], "Tommy Kelly");
 
 console.log(supremePizza, chessePizza);
 console.log(supremePizza.constructor);
@@ -37,4 +40,45 @@ function tellMeAboutButton() {
 button1.addEventListener("click", tellMeAboutButton);
 button2.addEventListener("click", tellMeAboutButton);
 
+const person = {
+  name: "Cookie Kelly",
+  sayHi() {
+    return `Hey ${this.name}`;
+  }
+};
 
+// const sayHi = person.sayHi;
+
+// Running the sayHi, this is equal to the window
+// There is nothing to the left hand side for it to be bound to (sayHi();)
+
+// const sayHi = person.sayHi.bind(person);
+
+// this changes what this is equal to by binding it to another object (person)
+
+const caroline = { name: "Caroline" };
+const sayHi = person.sayHi.bind(caroline);
+// we can bind the function to caroline and when you run it returns hey caroline
+// bind is a method which lives on all functions
+// change the this keyword to be equal too in this case a different object (caroline)
+
+const $ = document.querySelector.bind(document);
+const lookFor = document.querySelectorAll.bind(document);
+// by calling bind against querySelector, we say when the $
+//  function is run, use 'document' as the 'this' value
+
+const wrapper = document.querySelector(".wrapper");
+const p = wrapper.querySelector("p");
+console.log(p);
+console.log(lookFor("p"));
+
+const bill = {
+  total: 1000,
+  calculate: function(taxRate) {
+    return this.total + this.total * taxRate;
+  }
+};
+const total = bill.calculate(0.21);
+console.log(total);
+const calc = bill.calculate.bind({ total: 758 });
+console.log(calc(0.21));
