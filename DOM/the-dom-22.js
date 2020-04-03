b16f2eeabe3900106add90c92040f322861f8aad
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-03 13:32:58 +0100

    Element Properties, get & setting. Methods

--- DOM/the-dom-22.js
+++ DOM/the-dom-22.js
@@ -10,4 +10,27 @@ console.log(imgs);
 console.log(item2);
 console.log(item2Image);
 
+const heading = document.querySelector("h2");
+// dir shows the object properties instead of the element itself
+
+console.dir(heading.textContent);
+// thats a getter - tells you the information held in heading
+
+console.log(heading.textContent);
+heading.textContent = "this changes the heading";
+// this is a setter - sets the textcontent property on that element
+console.log(heading.textContent);
+
+// innerText v textContent - text content sees hidden and css properties
+
+console.log(heading.innerHTML);
+
+const pintList = document.querySelector(".pints");
+console.log(pintList.textContent);
+
+// pintList.textContent = `${pintList.textContent}- 1 pint`;
+// console.log(pintList.textContent);
+
+pintList.insertAdjacentText('beforeend','- 1 pint')
+console.log(pintList.textContent);
 
