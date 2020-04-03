e9525dc9c7716a352c3866a242b4c8bd18035040
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-03 12:36:06 +0100

    document, tag selectors

--- DOM/the-dom-22.js
+++ DOM/the-dom-22.js
@@ -1,2 +1,13 @@
 const p = document.querySelector("p");
+const divs = document.querySelector("div");
+const imgs = document.querySelectorAll(".item img");
+const item2 = document.querySelector(".item2");
+const item2Image = item2.querySelector("img");
+
 console.log(p);
+console.log(divs);
+console.log(imgs);
+console.log(item2);
+console.log(item2Image);
+
+
