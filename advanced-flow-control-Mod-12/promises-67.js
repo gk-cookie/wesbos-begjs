8a8ea1668851e45d98a0716716832b5f4c605d7c
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-06 11:08:58 +0100

    concurrent & .race

--- advanced-flow-control-Mod-12/promises-67.js
+++ advanced-flow-control-Mod-12/promises-67.js
@@ -1,27 +1,84 @@
-function makePizza(toppings) {
+function makePizza(toppings = []) {
   const pizzaPromise = new Promise(function (resolve, reject) {
+    const amountOfTimeToBake = 500 + toppings.length * 200;
     setTimeout(function () {
       // Promise holds 2 arguments 1. resolve 2 reject
       // when you are ready, you can resolve this promise
       //  if something went wrong, you can reject the promise
 
       resolve(`Here is your pizza with the toppings ${toppings.join(" ")}`);
-    }, 1000);
+    }, amountOfTimeToBake);
   });
   return pizzaPromise;
 }
 
-makePizza(["Pepperoni"])
-  .then(function (pizza) {
-    console.log(pizza);
-    return makePizza(["Ham", "Cheese"]);
-  })
-  .then(function (pizza) {
-    console.log(pizza);
-    return makePizza(["Blackened Chicken", "BBQ Sauce"]);
-  })
-  .then(function (pizza) {
-    console.log(pizza);
-  });
+// makePizza(["Pepperoni"])
+//   .then(function (pizza) {
+//     console.log(pizza);
+//     return makePizza(["Ham", "Cheese"]);
+//   })
+//   .then(function (pizza) {
+//     console.log(pizza);
+//     return makePizza(["Blackened Chicken", "BBQ Sauce"]);
+//   })
+//   .then(function (pizza) {
+//     console.log(pizza);
+//     return makePizza(["Five Cheese"]);
+//   })
+//   .then(function (pizza) {
+//     console.log(pizza);
+//     return makePizza([
+//       "feta",
+//       "cheddar",
+//       "peppers",
+//       "spinach",
+//       "ham",
+//       "swiss",
+//       "Blackened Chicken",
+//       "BBQ Sauce, pepperoni",
+//       "mushrooms",
+//       "onions",
+//     ]).then((pizza) => {
+//       console.log(pizza);
+//     });
+//   });
 
-const canadianPromise = makePizza(["pepperoni", "mushrooms", "onions"]);
+//   run them concurrently
+
+const pizzaPromise1 = makePizza(["Blackened Chicken", "BBQ Sauce"]);
+const pizzaPromise2 = makePizza(["Ham", "Cheese"]);
+const pizzaPromise3 = makePizza([
+  "feta",
+  "cheddar",
+  "peppers",
+  "spinach",
+  "ham",
+  "swiss",
+  "Blackened Chicken",
+  "BBQ Sauce, pepperoni",
+  "mushrooms",
+  "onions",
+]);
+
+const dinnerPromise = Promise.all([
+  pizzaPromise1,
+  pizzaPromise2,
+  pizzaPromise3,
+]);
+// dinnerPromise.then((pizzas) => {
+//   console.log(pizzas);
+// });
+
+dinnerPromise.then(function (pizzas) {
+  const [texas, hamAndCheese, Supreme] = pizzas;
+  console.log(texas, hamAndCheese, Supreme);
+});
+
+const firstPizzaPromise = Promise.race([pizzaPromise1, pizzaPromise2, pizzaPromise3]);
+
+firstPizzaPromise.then(pizza => {
+    console.log('You must be hungry, here is the first one ready');
+    console.log(pizza);
+    
+    
+})
