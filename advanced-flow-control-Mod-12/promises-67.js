f677a4abe80137829b4bc74733a233940a730638
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-06 10:33:16 +0100

    Created a chain of new promises

--- advanced-flow-control-Mod-12/promises-67.js
+++ advanced-flow-control-Mod-12/promises-67.js
@@ -1,17 +1,27 @@
 function makePizza(toppings) {
   const pizzaPromise = new Promise(function (resolve, reject) {
-    // Promise holds 2 arguments 1. resolve 2 reject
-    // when you are ready, you can resolve this promise
-    resolve(`Here is your pizza with the toppings ${toppings.join(" ")}`);
-    //  if something went wrong, you can reject the promise
+    setTimeout(function () {
+      // Promise holds 2 arguments 1. resolve 2 reject
+      // when you are ready, you can resolve this promise
+      //  if something went wrong, you can reject the promise
+
+      resolve(`Here is your pizza with the toppings ${toppings.join(" ")}`);
+    }, 1000);
   });
   return pizzaPromise;
 }
 
-const pepperoniPromise = makePizza(["Pepperoni"]);
-const canadianPromise = makePizza(["pepperoni", "mushrooms", "onions"]);
+makePizza(["Pepperoni"])
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza(["Ham", "Cheese"]);
+  })
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza(["Blackened Chicken", "BBQ Sauce"]);
+  })
+  .then(function (pizza) {
+    console.log(pizza);
+  });
 
-pepperoniPromise.then(function (pizza) {
-  console.log("Here she be");
-  console.log(pizza);
-});
+const canadianPromise = makePizza(["pepperoni", "mushrooms", "onions"]);
