5110c8cd698328e9b98c2508ff0307a04f7432e3
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-06 17:12:30 +0100

    added submit button and input

--- advanced-flow-control-Mod-12/async-await-promt-72.js
+++ advanced-flow-control-Mod-12/async-await-promt-72.js
@@ -1,17 +1,26 @@
+function wait(ms = 0) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 function ask(options) {
-  return new Promise(function (resolve) {
+  return new Promise(async function (resolve) {
     const popup = document.createElement("form");
     popup.classList.add("popup");
     popup.insertAdjacentHTML(
       "afterbegin",
       `
 <fieldset>
-<label>${options.title}</label>
-</fieldset>
-
-`
+    <label>${options.title}</label>
+    <input type="text" name="input"/>
+    <button type="submit">Submit</button>
+</fieldset>`
     );
-    console.log(popup);
-    
+    if (options.cancel) {
+      const skipButton = document.createElement("button");
+      skipButton.type = "button";
+      skipButton.textContent = "Cancel";
+    }
+    document.body.appendChild(popup);
+    await wait(50);
+    popup.classList.add("open");
   });
 }
