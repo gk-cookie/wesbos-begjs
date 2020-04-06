41b1b0f90874f9eb58fc2325c5bc11787a359055
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-06 10:15:02 +0100

    Passed in arguments and then accessed it with then

--- advanced-flow-control-Mod-12/promises-67.js
+++ advanced-flow-control-Mod-12/promises-67.js
@@ -1,13 +1,17 @@
-function makePizza() {
+function makePizza(toppings) {
   const pizzaPromise = new Promise(function (resolve, reject) {
     // Promise holds 2 arguments 1. resolve 2 reject
     // when you are ready, you can resolve this promise
-    resolve('pizza');
+    resolve(`Here is your pizza with the toppings ${toppings.join(" ")}`);
     //  if something went wrong, you can reject the promise
-
   });
   return pizzaPromise;
 }
 
-const pizza = makePizza();
-console.log(pizza);
+const pepperoniPromise = makePizza(["Pepperoni"]);
+const canadianPromise = makePizza(["pepperoni", "mushrooms", "onions"]);
+
+pepperoniPromise.then(function (pizza) {
+  console.log("Here she be");
+  console.log(pizza);
+});
