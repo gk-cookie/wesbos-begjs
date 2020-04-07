2a34de8884208b91f8d4ae7c4fae6a2f0bc9549f
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-07 13:01:22 +0100

    Updated Shortcuts - async typer using recursion

--- advanced-flow-control-Mod-12/async-typer-73.js
+++ advanced-flow-control-Mod-12/async-typer-73.js
@@ -6,18 +6,40 @@ function getRandomBetween(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-async function draw(el) {
+// Creating async typer using for of loop
+// async function draw(el) {
+//   const text = el.textContent;
+//   let soFar = "";
+//   for (const letter of text) {
+//     console.log(letter);
+//     soFar += letter;
+//     console.log(soFar);
+//     el.textContent = soFar;
+//     const { typeMin, typeMax } = el.dataset;
+//     const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//     await wait(amountOfTimeToWait);
+//   }
+// }
+
+// Recursion
+
+function draw(el) {
+  let index = 1;
   const text = el.textContent;
-  let soFar = "";
-  for (const letter of text) {
-    console.log(letter);
-    soFar += letter;
-    console.log(soFar);
-    el.textContent = soFar;
-    const { typeMin, typeMax } = el.dataset;
-    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
-    await wait(amountOfTimeToWait);
+  const { typeMin, typeMax } = el.dataset;
+  async function drawLetter() {
+    el.textContent = text.slice(0, index);
+    index += 1;
+    //     // exit condition
+
+    if (index <= text.length) {
+      const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+      await wait(amountOfTimeToWait);
+      drawLetter();
+    }
   }
+  //     // when this function draw() runs, kick off drawLetter
+  drawLetter();
 }
 
 const els = document.querySelectorAll("[data-type]");
