8b6f162fbc0d3aaf45df878544df6ae0a1cbe4a7
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-07 12:18:08 +0100

    for of loop to add letter to text

--- advanced-flow-control-Mod-12/async-typer-73.js
+++ advanced-flow-control-Mod-12/async-typer-73.js
@@ -1,3 +1,21 @@
 function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+function getRandomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function draw(el) {
+  const text = el.textContent;
+  let soFar = "";
+  for (const letter of text) {
+    console.log(letter);
+    soFar += letter;
+    console.log(soFar);
+    
+  }
+}
+
+const els = document.querySelectorAll("[data-type]");
+els.forEach(draw);
