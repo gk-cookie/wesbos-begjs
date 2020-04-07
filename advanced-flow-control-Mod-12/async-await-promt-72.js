a9d5f64abed38388432c0b932768d59497b8ce7b
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-07 10:54:22 +0100

    created array with answers

--- advanced-flow-control-Mod-12/async-await-promt-72.js
+++ advanced-flow-control-Mod-12/async-await-promt-72.js
@@ -29,11 +29,14 @@ function ask(options) {
       skipButton.type = "button";
       skipButton.textContent = "Cancel";
       popup.firstElementChild.appendChild(skipButton);
-      skipButton.addEventListener('click', function () {
+      skipButton.addEventListener(
+        "click",
+        function () {
           resolve(null);
           destroyPopup(popup);
-      },
-      {once: true})
+        },
+        { once: true }
+      );
     }
     popup.addEventListener(
       "submit",
@@ -53,7 +56,7 @@ function ask(options) {
 
 async function askQuestion(e) {
   const button = e.currentTarget;
-  const cancel =  'cancel' in button.dataset;
+  const cancel = "cancel" in button.dataset;
   const answer = await ask({
     title: button.dataset.question,
     cancel,
@@ -64,3 +67,34 @@ async function askQuestion(e) {
 const buttons = document.querySelectorAll("[data-question]");
 buttons.forEach((button) => button.addEventListener("click", askQuestion));
 
+const questions = [
+  { title: "What is your name?" },
+  { title: "What is your age?", cancel: true },
+  { title: "Thats is your dogs name?" },
+];
+
+console.log(questions);
+
+async function asyncMap(array, callback) {
+  const results = [];
+  for (const item of array) {
+    const result = await callback(item);
+    results.push(result);
+  }
+  return results;
+}
+
+async function go() {
+    const answers = await asyncMap(questions, ask);
+    console.log(answers);
+    
+}
+go();
+// async function askMany() {
+//   for (const question of questions) {
+//     const answer = await ask(question);
+//     console.log(answer);
+//   }
+// }
+
+// askMany();
