696e279cab8ce536bb11091976748ff9c831e863
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-07 09:48:12 +0100

    Remove the popup when submitted

--- advanced-flow-control-Mod-12/async-await-promt-72.js
+++ advanced-flow-control-Mod-12/async-await-promt-72.js
@@ -1,6 +1,15 @@
 function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+async function destroyPopup(popup) {
+  let myPopup = popup;
+  popup.classList.remove("open");
+  await wait(1000);
+  popup.remove();
+  myPopup = null;
+}
+
 function ask(options) {
   return new Promise(async function (resolve) {
     const popup = document.createElement("form");
@@ -8,17 +17,29 @@ function ask(options) {
     popup.insertAdjacentHTML(
       "afterbegin",
       `
-<fieldset>
-    <label>${options.title}</label>
-    <input type="text" name="input"/>
-    <button type="submit">Submit</button>
-</fieldset>`
+    <fieldset>
+        <label>${options.title}</label>
+        <input type="text" name="input"/>
+        <button type="submit">Submit</button>
+    </fieldset>`
     );
+
     if (options.cancel) {
       const skipButton = document.createElement("button");
       skipButton.type = "button";
       skipButton.textContent = "Cancel";
+      popup.firstElementChild.appendChild(skipButton);
     }
+    popup.addEventListener(
+      "submit",
+      function (e) {
+        e.preventDefault();
+        resolve(e.target.input.value);
+        destroyPopup(popup);
+      },
+      { once: true }
+    );
+
     document.body.appendChild(popup);
     await wait(50);
     popup.classList.add("open");
