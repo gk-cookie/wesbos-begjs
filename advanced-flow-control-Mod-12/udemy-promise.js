c604ffda814d0ea17f68b66687e293c27b0a81f3
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-08 12:58:44 +0100

    Promise All

--- advanced-flow-control-Mod-12/udemy-promise.js
+++ advanced-flow-control-Mod-12/udemy-promise.js
@@ -1,9 +1,39 @@
 const promise = new Promise((resolve, reject) => {
-if (true) {
-    resolve('stuff worked')
-} else {
-    reject('error, it broke')
-}
+  if (true) {
+    resolve("stuff worked");
+  } else {
+    reject("error, it broke");
+  }
+});
+
+// promise.then(result => console.log(result));
+
+promise
+  .then((result) => result + "!")
+  .then((result2) => {
+    throw Error;
+    console.log(result2);
+  })
+  .catch(() => console.log("Errrroorrr"));
+
+//   .catch catches the error anywhere between the .then
+//  .catch is always at the end
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "Hiii");
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "Promey 3");
+});
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, "Are you looking for me");
+});
+
+Promise.all([promise, promise2, promise3, promise4])
+.then(values => {
+    console.log(values);
+    
 })
 
-promise.then(result => console.log(result));
+// it waits until all the promises are resolved and then logged out the value
