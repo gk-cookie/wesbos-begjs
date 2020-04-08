481fc7a318edbde0a3a029a5c3c5adeb96cbd7f3
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-08 13:54:36 +0100

    using json

--- advanced-flow-control-Mod-12/udemy-promise.js
+++ advanced-flow-control-Mod-12/udemy-promise.js
@@ -30,10 +30,23 @@ const promise4 = new Promise((resolve, reject) => {
   setTimeout(resolve, 3000, "Are you looking for me");
 });
 
-Promise.all([promise, promise2, promise3, promise4])
-.then(values => {
-    console.log(values);
-    
-})
+Promise.all([promise, promise2, promise3, promise4]).then((values) => {
+  console.log(values);
+});
 
 // it waits until all the promises are resolved and then logged out the value
+
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(resp=> resp.json());
+  }))
+  .then(results => {
+  console.log(results[0]);
+  console.log(results[1]);
+  console.log(results[2]);
+});
