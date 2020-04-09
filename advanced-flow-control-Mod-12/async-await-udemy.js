d020a35a938f2c43a1d6dba71ce23cf955d4d605
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-09 16:19:04 +0100

    async returned as arrays

--- advanced-flow-control-Mod-12/async-await-udemy.js
+++ advanced-flow-control-Mod-12/async-await-udemy.js
@@ -4,10 +4,37 @@
 //   })
 //   .then(console.log);
 
-async function fetchUsers() {
-  const resp = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await resp.json();
-  console.log(data);
-}
+// async function fetchUsers() {
+//   const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await resp.json();
+//   console.log(data);
+// }
 
-fetchUsers();
+// fetchUsers();
+
+const urls = [
+  "https://reqres.in/api/users",
+  "https://reqres.in/api/posts",
+  "https://reqres.in/api/albums",
+];
+
+const promises = urls.map((url) => {
+  return fetch(url).then((resp) => resp.json());
+});
+
+console.log(promises);
+
+Promise.all(promises).then((results) => {
+  console.log(results[0]);
+  console.log(results[1]);
+  console.log(results[2]);
+});
+
+const getData = async function () {
+  const [users, posts, albums] = await promises;
+  console.log("users", users);
+  console.log("posts", posts);
+  console.log("albums", albums);
+};
+
+getData();
