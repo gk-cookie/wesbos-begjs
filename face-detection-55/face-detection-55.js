72d69db457323270cb988480f07ef1a1338c89a1
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-09 12:06:53 +0100

    opened webcam access

--- face-detection-55/face-detection-55.js
+++ face-detection-55/face-detection-55.js
@@ -7,5 +7,13 @@ const faceCanvas = document.querySelector(".face");
 const factCtx = canvas.getContext("2d");
 
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
 
+async function populateVideo() {
+  const stream = await  navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+}
+
+populateVideo();
