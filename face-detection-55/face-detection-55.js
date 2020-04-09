eb26e3a11dc21a9f8df4355d6df65d6dc96ca6f3
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-09 12:36:11 +0100

    accessed face detector, put canvas over webcam

--- face-detection-55/face-detection-55.js
+++ face-detection-55/face-detection-55.js
@@ -9,11 +9,34 @@ const factCtx = canvas.getContext("2d");
 const faceDetector = new window.FaceDetector();
 
 async function populateVideo() {
-  const stream = await  navigator.mediaDevices.getUserMedia({
-    video: { width: 1280, height: 720 },
-  });
-  video.srcObject = stream;
-  await video.play();
+    const stream = await navigator.mediaDevices.getUserMedia({
+        // video: { width: 1280, height: 720 },
+        video: true,
+    });
+    video.srcObject = stream;
+    await video.play();
+    
+    //   size the canvases to be same size as the video
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    faceCanvas.width = video.videoWidth;
+    faceCanvas.height = video.videoHeight;
 }
 
-populateVideo();
+async function detect() {
+    const faces = await faceDetector.detect(video);
+    console.log(faces);
+    faces.forEach(drawFace);
+    //    ask the browser when the next animation frame is and tell them to run detect
+    requestAnimationFrame(detect);
+}
+
+function drawFace(face) {
+    const { width, height, top, left } = face.boundingBox;
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(left, top, width, height);
+}
+
+populateVideo().then(detect);
