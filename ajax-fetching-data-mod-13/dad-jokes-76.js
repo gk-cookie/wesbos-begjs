05e04bb1c56cc3bacecf13e308d1fe6f1d980d38
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-15 15:24:19 +0100

    working joke button

--- ajax-fetching-data-mod-13/dad-jokes-76.js
+++ ajax-fetching-data-mod-13/dad-jokes-76.js
@@ -1,1 +1,33 @@
-console.log('works');
+const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  
+}
+
+jokeButton.addEventListener('click', handleClick);
