2b34b25af4fc2a12105491207d3cee2f822e2980
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-15 13:20:39 +0100

    added grid in the html to display results

--- ajax-fetching-data-mod-13/cors-recipes-75.js
+++ ajax-fetching-data-mod-13/cors-recipes-75.js
@@ -2,8 +2,8 @@
 // Cross (not allowed to share data with other websites)
 // Origin (you can grab info from your own domain name)
 // (by default websites(domain name) cannot talk to each other)
-// R
-// S
+// Resource
+// Sharing
 
 // CORS Policy - wesbos.com is allowed to ask for data and we can return it
 // Policy must come from server where data is requested from
@@ -17,28 +17,46 @@
 //                  ↓
 //                  recipepuppy
 
-const baseEndpoint = `http://www.recipepuppy.com/api/`;
+const baseEndpoint = 'http://www.recipepuppy.com/api';
 const proxy = `https://cors-anywhere.herokuapp.com/`;
-const form = document.querySelector("form.search");
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
   const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const data = await res.json();
-return data;
+  return data;
 }
 
 async function handleSubmit(event) {
   event.preventDefault();
-  console.log(form.query.value);
-  
   const el = event.currentTarget;
-  el.submit.disabled = true;
+  console.log(form.query.value);
+  fetchAndDisplay(form.query.value);
+}
 
-  const recipes = await fetchRecipes(el.query.value);
+async function fetchAndDisplay(query) {
+  form.submit.disabled = true;
+  const recipes = await fetchRecipes(query);
   console.log(recipes);
-  el.submit.disabled = false;
-  
-};
+  form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
 
-form.addEventListener("submit", handleSubmit);
-fetchRecipes("pizza");
+function displayRecipes(recipes) {
+  console.log('Creating HTML');
+  const html = recipes.map(
+    recipe => `<div class="recipe">
+      <h2>${recipe.title}</h2>
+      <p>${recipe.ingredients}</p>
+      ${recipe.thumbnail &&
+        `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+      <a href="${recipe.href}">View Recipe →</a>
+    </div>`
+  );
+  recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+// on page load run it with pizza
+fetchAndDisplay('pizza');
