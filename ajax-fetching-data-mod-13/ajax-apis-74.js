82581eb4713ec2bc8f17f4e11d9d371c4ba4735e
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-15 10:35:36 +0100

    converted to async function

--- ajax-fetching-data-mod-13/ajax-apis-74.js
+++ ajax-fetching-data-mod-13/ajax-apis-74.js
@@ -4,23 +4,23 @@
 // And
 // Xml
 
+const endpoint = "https://api.github.com/users/wesbos";
+const userEl = document.querySelector(".user");
+
 function handleError(err) {
   console.log("an error");
   console.log(err);
 }
-const endpoint = "https://api.github.com/users/wesbos";
+async function displayUser(username) {
+  userEl.textContent = `loading...`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
 
-const cookPromise = fetch(endpoint);
-cookPromise
-  .then((response) => {
-    return response.json();
-    console.log(response);
-  })
-  .then((data) => {
-    console.log(data);
-    console.log(data.bio);
-    console.log(data.name);
-    console.log(data.location);
-    
-  })
-  .catch(handleError);
+  console.log(data);
+  console.log(data.bio);
+  console.log(data.name);
+  console.log(data.location);
+  userEl.textContent = `${data.name} - ${data.blog}`;
+}
+
+displayUser();
