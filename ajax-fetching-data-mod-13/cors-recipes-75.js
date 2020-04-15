b9f183bd034cf1f142751739d375afe4fce5250d
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-15 12:43:11 +0100

    added proxy for CORS

--- ajax-fetching-data-mod-13/cors-recipes-75.js
+++ ajax-fetching-data-mod-13/cors-recipes-75.js
@@ -8,11 +8,23 @@
 // CORS Policy - wesbos.com is allowed to ask for data and we can return it
 // Policy must come from server where data is requested from
 
+// localhost        localhost
+// ↑                ↑
+// wont work        ↓
+// ↓                proxy
+// recipepuppy      ↑
+//                  will work
+//                  ↓
+//                  recipepuppy
+
 const baseEndpoint = `http://www.recipepuppy.com/api/`;
+const proxy = `https://cors-anywhere.herokuapp.com/`;
 
 async function fetchRecipes(query) {
-  const res = await fetch(`${baseEndpoint}?q=${query}`);
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const data = await res.json();
+  console.log(data);
 }
+
 
 fetchRecipes("pizza");
