bc51f525a052b7975ef03d311a8366d9c3d2866f
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-15 10:18:39 +0100

    fectch, .json and grabing some data from api

--- ajax-fetching-data-mod-13/ajax-apis-74.js
+++ ajax-fetching-data-mod-13/ajax-apis-74.js
@@ -4,7 +4,23 @@
 // And
 // Xml
 
+function handleError(err) {
+  console.log("an error");
+  console.log(err);
+}
 const endpoint = "https://api.github.com/users/wesbos";
 
 const cookPromise = fetch(endpoint);
-console.log(cookPromise);
+cookPromise
+  .then((response) => {
+    return response.json();
+    console.log(response);
+  })
+  .then((data) => {
+    console.log(data);
+    console.log(data.bio);
+    console.log(data.name);
+    console.log(data.location);
+    
+  })
+  .catch(handleError);
