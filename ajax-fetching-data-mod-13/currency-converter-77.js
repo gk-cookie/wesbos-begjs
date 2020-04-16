356e0caf44e2349106c879efe9ac93d18eef44ce
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-16 11:43:42 +0100

    added currencies to options html

--- ajax-fetching-data-mod-13/currency-converter-77.js
+++ ajax-fetching-data-mod-13/currency-converter-77.js
@@ -1,37 +1,45 @@
 const currencies = {
-    USD: 'United States Dollar',
-    AUD: 'Australian Dollar',
-    BGN: 'Bulgarian Lev',
-    BRL: 'Brazilian Real',
-    CAD: 'Canadian Dollar',
-    CHF: 'Swiss Franc',
-    CNY: 'Chinese Yuan',
-    CZK: 'Czech Republic Koruna',
-    DKK: 'Danish Krone',
-    GBP: 'British Pound Sterling',
-    HKD: 'Hong Kong Dollar',
-    HRK: 'Croatian Kuna',
-    HUF: 'Hungarian Forint',
-    IDR: 'Indonesian Rupiah',
-    ILS: 'Israeli New Sheqel',
-    INR: 'Indian Rupee',
-    JPY: 'Japanese Yen',
-    KRW: 'South Korean Won',
-    MXN: 'Mexican Peso',
-    MYR: 'Malaysian Ringgit',
-    NOK: 'Norwegian Krone',
-    NZD: 'New Zealand Dollar',
-    PHP: 'Philippine Peso',
-    PLN: 'Polish Zloty',
-    RON: 'Romanian Leu',
-    RUB: 'Russian Ruble',
-    SEK: 'Swedish Krona',
-    SGD: 'Singapore Dollar',
-    THB: 'Thai Baht',
-    TRY: 'Turkish Lira',
-    ZAR: 'South African Rand',
-    EUR: 'Euro',
-  };
+  USD: "United States Dollar",
+  AUD: "Australian Dollar",
+  BGN: "Bulgarian Lev",
+  BRL: "Brazilian Real",
+  CAD: "Canadian Dollar",
+  CHF: "Swiss Franc",
+  CNY: "Chinese Yuan",
+  CZK: "Czech Republic Koruna",
+  DKK: "Danish Krone",
+  GBP: "British Pound Sterling",
+  HKD: "Hong Kong Dollar",
+  HRK: "Croatian Kuna",
+  HUF: "Hungarian Forint",
+  IDR: "Indonesian Rupiah",
+  ILS: "Israeli New Sheqel",
+  INR: "Indian Rupee",
+  JPY: "Japanese Yen",
+  KRW: "South Korean Won",
+  MXN: "Mexican Peso",
+  MYR: "Malaysian Ringgit",
+  NOK: "Norwegian Krone",
+  NZD: "New Zealand Dollar",
+  PHP: "Philippine Peso",
+  PLN: "Polish Zloty",
+  RON: "Romanian Leu",
+  RUB: "Russian Ruble",
+  SEK: "Swedish Krona",
+  SGD: "Singapore Dollar",
+  THB: "Thai Baht",
+  TRY: "Turkish Lira",
+  ZAR: "South African Rand",
+  EUR: "Euro",
+};
 
+function generateOptions(options) {
+  return Object.entries(options)
+  .map(
+      ([currencyCode, currencyName]) => 
+    `<option value ="${currencyCode}">${currencyCode} - ${currencyName}</option>`
+  ).join('');
+}
 
-  
+const optionsHTML = generateOptions(currencies);
+console.log(optionsHTML);
