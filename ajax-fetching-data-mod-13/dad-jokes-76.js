f5e573504d40784becc3e75dcc291b6653a03a3a
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-16 10:05:12 +0100

    changing button text and not repeating same text

--- ajax-fetching-data-mod-13/dad-jokes-76.js
+++ ajax-fetching-data-mod-13/dad-jokes-76.js
@@ -1,33 +1,45 @@
-const jokeButton = document.querySelector('.getJoke');
-const jokeButtonSpan = jokeButton.querySelector('.jokeText');
-const jokeHolder = document.querySelector('.joke p');
-const loader = document.querySelector('.loader');
+const jokeButton = document.querySelector(".getJoke");
+const jokeButtonSpan = jokeButton.querySelector(".jokeText");
+const jokeHolder = document.querySelector(".joke p");
+const loader = document.querySelector(".loader");
 
 const buttonText = [
-  'Ugh.',
-  '🤦🏻‍♂️',
-  'omg dad.',
-  'you are the worst',
-  'seriously',
-  'stop it.',
-  'please stop',
-  'that was the worst one',
+  "Ugh.",
+  "🤦🏻‍♂️",
+  "omg dad.",
+  "you are the worst",
+  "seriously",
+  "stop it.",
+  "please stop",
+  "that was the worst one",
 ];
 
 async function fetchJoke() {
-  const response = await fetch('https://icanhazdadjoke.com', {
+  const response = await fetch("https://icanhazdadjoke.com", {
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   const data = await response.json();
   return data;
 }
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log('we used that one before');
+    return randomItemFromArray(arr, not);
+    
+  }
+  return item;
+}
 
 async function handleClick() {
   const { joke } = await fetchJoke();
   jokeHolder.textContent = joke;
-  
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
 }
 
-jokeButton.addEventListener('click', handleClick);
+jokeButton.addEventListener("click", handleClick);
