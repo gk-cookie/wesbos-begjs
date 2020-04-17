7da7abcb8923a38d2dc7bb3faaf5c62702da0446
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-17 12:52:53 +0100

    import everything with *

--- modules-mod-14/cook.js
+++ modules-mod-14/cook.js
@@ -5,3 +5,10 @@ const person = {
 // default module can only be used once
 
 export default person;
+
+export const dog = 'Sally';
+export const food = 'Pizza';
+export function eat() {
+    console.log('Chomp Chomp');
+    
+}
--- modules-mod-14/modules-78.js
+++ modules-mod-14/modules-78.js
@@ -1,10 +1,18 @@
-import { returnHi, middle, last } from './utils-78.js';
-import cook from './cook.js';
-console.log(cook);
+import { returnHi as sayHi, middle, last } from './utils-78.js';
+// can be renamed by using as
 
+// import jimmy from './cook.js';
+// you can name the default exports whatever you like
+// named in curly brackets, default outside
+ 
+import * as everything from './cook.js';
 const name = 'Cook';
+// you can use a star to import everything
 
-console.log(returnHi(name));
 
-console.log('its working');
+// console.log(jimmy);
+// console.log(returnHi(name));
+console.log(sayHi(name));
+
 console.log(middle, last);
+console.log(everything);
