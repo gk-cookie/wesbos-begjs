53eb8434d991900abdde04d50830ae6ef4bb8a47
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-20 10:14:32 +0100

    converter working

--- modules-mod-14/currency-modules/currency-modules-79.js
+++ modules-mod-14/currency-modules/currency-modules-79.js
@@ -2,12 +2,9 @@ import { generateOptions } from "./utils-79.js";
 import currencies from "./currencies-79.js";
 import { handleInput } from "./handlers-79.js";
 import { convert } from "./lib-79.js";
-import { fromSelect, toSelect } from "./elements-79.js"
-
+import { fromSelect, toSelect } from "./elements-79.js";
 
 const form = document.querySelector(".app form ");
-const endpoint = "https://api.exchangeratesapi.io/latest";
-const ratesByBase = {};
 
 const optionsHTML = generateOptions(currencies);
 
--- modules-mod-14/currency-modules/lib-79.js
+++ modules-mod-14/currency-modules/lib-79.js
@@ -1,3 +1,6 @@
+const endpoint = "https://api.exchangeratesapi.io/latest";
+const ratesByBase = {};
+
 export async function fetchRates(base = "USD") {
     const res = await fetch(`${endpoint}?base=${base}`);
     const rates = await res.json();
