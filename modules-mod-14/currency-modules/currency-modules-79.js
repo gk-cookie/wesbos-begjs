65b0387f071b9a4259a6613e0ea0ff48ed1e075b
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-20 11:00:09 +0100

    Dad Jokes module change

--- modules-mod-14/currency-modules/currency-modules-79.js
+++ modules-mod-14/currency-modules/currency-modules-79.js
@@ -3,6 +3,6 @@ import currencies from "./currencies-79.js";
 import { handleInput } from "./handlers-79.js";
 import { convert } from "./lib-79.js";
 import { fromSelect, toSelect } from "./elements-79.js";
-import { init } from "./lib-79.js"
+import { init } from "./lib-79.js";
 
 init();
