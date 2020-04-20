adadda997beeaba3d5a2f89ea7942ea1dfb9794e
gk-cookie <46975236+gk-cookie@users.noreply.github.com>
2020-04-20 10:46:40 +0100

    added init function to run on page download

--- modules-mod-14/currency-modules/currency-modules-79.js
+++ modules-mod-14/currency-modules/currency-modules-79.js
@@ -3,11 +3,6 @@ import currencies from "./currencies-79.js";
 import { handleInput } from "./handlers-79.js";
 import { convert } from "./lib-79.js";
 import { fromSelect, toSelect } from "./elements-79.js";
+import { init } from "./lib-79.js"
 
-const form = document.querySelector(".app form ");
-
-const optionsHTML = generateOptions(currencies);
-
-fromSelect.innerHTML = optionsHTML;
-toSelect.innerHTML = optionsHTML;
-form.addEventListener("input", handleInput);
+init();
--- modules-mod-14/currency-modules/lib-79.js
+++ modules-mod-14/currency-modules/lib-79.js
@@ -1,23 +1,34 @@
+import { generateOptions } from "./utils-79.js";
+import { fromSelect, toSelect } from "./elements-79.js";
+import { currencies } from "./currencies-79.js";
+import { handleInput } from "./handlers-79.js";
 const endpoint = "https://api.exchangeratesapi.io/latest";
 const ratesByBase = {};
 
 export async function fetchRates(base = "USD") {
-    const res = await fetch(`${endpoint}?base=${base}`);
-    const rates = await res.json();
-    return rates;
-  }
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates;
+}
 
-  export async function convert (amount, from, to) {
-    if (!ratesByBase[from])
-      console.log(`we dont have ${from} to convert to ${to}. Go get it`);
-    const rates = await fetchRates(from);
-    console.log(rates);
-    ratesByBase[from] = rates;
-  
-    const rate = ratesByBase[from].rates[to];
-    const convertedAmount = rate * amount;
-    console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
-    return convertedAmount;
-  }
+export async function convert(amount, from, to) {
+  if (!ratesByBase[from])
+    console.log(`we dont have ${from} to convert to ${to}. Go get it`);
+  const rates = await fetchRates(from);
+  console.log(rates);
+  ratesByBase[from] = rates;
 
-  
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = rate * amount;
+  console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+  return convertedAmount;
+}
+
+export function init() {
+  const form = document.querySelector(".app form ");
+  const optionsHTML = generateOptions(currencies);
+
+  fromSelect.innerHTML = optionsHTML;
+  toSelect.innerHTML = optionsHTML;
+  form.addEventListener("input", handleInput);
+}
